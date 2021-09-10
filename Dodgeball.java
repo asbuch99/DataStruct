f637cc4b881e5ba3d2548e3d40b39813f9673129
Anvay Buch <asbuch99@gmail.com>
2021-09-10 02:55:33 -0400

    Dodgeball Full

--- Dodgeball.java
+++ Dodgeball.java
@@ -1,157 +1,126 @@
+/*
+Anvay Buch
+Dodgeball Program 2
+This program runs a simulation of the full dodgeball game using only arrays.
+Received help from a friend that goes to Pitt
+*/
+
 import java.util.*;
 
-public class Dodgeball
-{
-   public static void main(String[] args) {
-   //number of players
-       int n;
-  
-   //prompt to input number of players
-       System.out.print("Enter the number of players > ");
-       Scanner sc = new Scanner(System.in);
-      
-       //Object of class Random for generating uniformly distributed random numbers
-       Random ran = new Random();
-      
-       //input until valid number of Players
-       //at least 2 players are required
-       do{
-       //input number of Players
-       n = sc.nextInt();
-      
-       if(n<2)
-       System.out.println("Enter number of players more than 1: ");
-       }while(n<2);
-      
-   //an array of Players
-       int Players[] = new int[n];
-      
-       //initialize array from 1 to n
-       for(int i=0; i<n;i++){
-       Players[i] = i+1;
-       }
-      
-       //to store result of a turn
-       String res[] = {"catch", "hit", "miss"};
-  
-   //Players left to play
-       int currPlayers = n;
-      
-       //initial thrower is the first Player
-       int thrower = Players[0];
-       //for storing the target
-       int target = -1;
-      
-       //to store result of turn
-       //0=catch, 1=hit, 2=miss
-       int r;
-      
-       //for result as String - "catch", "hit", "miss"
-       String result;
-      
-       //till condition is true
-       while(true){
-       //1st to current number of Players - 1
-       if (currPlayers == n && thrower == Players[0]){
-       //generating random target
-       target = Players[ran.nextInt(currPlayers-1) + 1];
-       }
-      
-       //0th index to current no. of Players-1
-       else if (thrower == -1 && target == -1){
-       //generating thrower
-       thrower = Players[ran.nextInt(currPlayers)];
-      
-       //generating target
-       do{
-       target = Players[ran.nextInt(currPlayers)];
-       }while(target==thrower);
-       }
-      
-       //generating result in Integer
-       r = ran.nextInt(3);
-       //accessing String equivalent result
-       result = res[r];
-      
-       //print the Players
-       for(int i=0;i<currPlayers;i++){
-      
-       //if the Player is a thrower add an asterisk
-       if(Players[i] == thrower)
-       System.out.print(Players[i] + "* ");
-      
-       else
-       //print only Player number
-       System.out.print(Players[i] + " ");
-      
-       }
-      
-       //print the target, result
-       System.out.println("\ntarget = player " + target + ",result = " + result + ".");
-       //result is catch
-       if (result.equals("catch")){
-       //remove thrower from Players
-       remove(Players, thrower);
-       //decrement number of Players
-       currPlayers--;
-       //the target becomes thrower
-       thrower = target;
-      
-       //if Players left
-       if(currPlayers!=1){
-       do{
-       //generating new target
-       target = Players[ran.nextInt(currPlayers)];
-       }while(target==thrower); //till target and thrower are not same
-       }
-       }
-      
-       //ball was hit
-       else if (result.equals("hit")){
-       //remove target from Players
-       remove(Players, target);
-       //assign -1 to both
-       thrower = -1;
-       target = -1;
-       //decrement number of Players
-       currPlayers--;
-       }
-      
-       //ball missed
-       else{
-       //find new thrower
-       thrower = Players[ran.nextInt(currPlayers)];
-       //enough Players left
-       if(currPlayers!=1){
-       do{
-       //generating new target
-       target = Players[ran.nextInt(currPlayers)];
-       }while(target==thrower);
-       }
-       }
-       //game over because only one Player left
-       if(currPlayers==1)
-       break;
-       }
-   }
-  
-   //method to remove a value from the array
-   public static void remove(int arr[], int val){
-   //traverse the array
-   for(int i=0; i<arr.length; i++)
-{
-//compare the value with elements
-//if value matches
-if(arr[i] == val)
-{
-//shift one position backwards
-for(int j=i; j<(arr.length-1); j++)
-{
-arr[j] = arr[j+1];
-}
-//break from the loop
-break;
-}
-}
-   }
+public class Dodgeball {
+    public static void main(String[] args) {
+        int n;
+
+        //prompt to input number of players
+        System.out.print("Enter the number of players: ");
+        Scanner sc = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        //input until valid number of Players
+
+        do {
+            n = sc.nextInt();
+
+            if (n < 2)
+                System.out.println("Enter the number of players: ");
+        } while (n < 2);
+
+        int Players[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            Players[i] = i + 1;
+        }
+
+        //To store result 
+        String res[] = {
+            "catch","hit","miss"
+        };
+
+        int currPlayers = n;
+
+        //Initial thrower is the player one
+        int thrower = Players[0];
+        int target = -1;
+        int r;
+
+        String result;
+
+        while (true) {
+            //First to current number of players minus 1 and generate a random target
+            if (currPlayers == n && thrower == Players[0]) {
+                target = Players[rand.nextInt(currPlayers - 1) + 1];
+            } else if (thrower == -1 && target == -1) {
+                thrower = Players[rand.nextInt(currPlayers)];
+
+                do {
+                    target = Players[rand.nextInt(currPlayers)];
+                } while (target == thrower);
+            }
+
+            r = rand.nextInt(3);
+            result = res[r];
+
+            //Print the players
+            for (int i = 0; i < currPlayers; i++) {
+
+                //If the player is a thrower add asterisk
+                if (Players[i] == thrower)
+                    System.out.print(Players[i] + "* ");
+
+                else
+                    System.out.print(Players[i] + " ");
+
+            }
+
+            //Print the target and result
+            System.out.println("\ntarget = player " + target + ",result = " + result + ".");
+            if (result.equals("catch")) {
+                removeval(Players, thrower);
+                currPlayers--;
+                thrower = target;
+
+                //If there are players left
+                if (currPlayers != 1) {
+                    do {
+                        target = Players[rand.nextInt(currPlayers)];
+                    } while (target == thrower);
+                }
+            }
+
+            //If the ball is hit
+            else if (result.equals("hit")) {
+                removeval(Players, target);
+                thrower = -1;
+                target = -1;
+                currPlayers--;
+            }
+
+            //If ball missed
+            else {
+                thrower = Players[rand.nextInt(currPlayers)];
+                if (currPlayers != 1) {
+                    do {
+                        target = Players[rand.nextInt(currPlayers)];
+                    } while (target == thrower);
+                }
+            }
+            //One player left means the game is over
+            if (currPlayers == 1)
+                break;
+        }
+    }
+
+    //To remove a value from the array
+    public static void removeval(int arr[], int val) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val) {
+                for (int j = i; j < (arr.length - 1); j++) {
+                    arr[j] = arr[j + 1];
+                }
+                break;
+            }
+        }
+    }
 }
